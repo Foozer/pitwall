3090eed783b19bea87a92c8704fb4d8d3de0d668
Iain Watt <iainskwatt@gmail.com>
2026-03-10 19:21:27 +0000

    working on main

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from pitwall!")
+import argparse
 
+def main():
+    parser = argparse.ArgumentParser(description="F1 info on the CLI")
+    parser.add_argument("next", help="give information about the next race weekend")
+    parser.add_argument("standings", help="Gives the current driver standings")
+    parser.add_argument("driver", help="gives information about the selected driver")
+
+    parser.add_argument("-v", "--verbose", action="store_true", help="incrase output verbosity")
+    args = parser.parse_args()
+    if args.verbose:
+        print("verbosity turned on")
+        print(args.echo)
+    else:
+        print(args.echo)
 
 if __name__ == "__main__":
     main()
